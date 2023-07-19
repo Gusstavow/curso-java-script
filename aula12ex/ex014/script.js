4b0e523494897695c0f4e8ae4986533e3a1aa3e9
Gustavo Ferreira <gustavobarreto2000@hotmail.com>
2023-07-19 01:13:25 -0300

    aula12 ex terminada

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,30 @@
-alert("Ola");
+
+
+function carregar() {
+    var msg = document.getElementById("msg");
+    var img = document.getElementById("imagem");
+    
+
+    var data = new Date();
+    var hora = data.getHours();
+    
+    msg.innerHTML = `Agora sao ${hora} hora(s).`;
+
+    if(hora >= 0 && hora <12){
+        //Bom dia
+        img.src = "./imagens/morning.png";
+        document.body.style.backgroundColor = "#FCE1C4"
+
+
+    }else if(hora >= 12 && hora <=18){
+        //boa tarde
+        img.src = "./imagens/afternoon.png"
+        document.body.style.backgroundColor = "#C96F4A"
+
+    }else{
+        //noite
+        img.src ="./imagens/night.png"
+        document.body.style.backgroundColor = "#00131A"
+    }
+
+}
