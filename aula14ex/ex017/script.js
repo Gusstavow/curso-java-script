c18427b9c6feba46d8a8a59874790ac3b0d6dbe0
Gustavo Ferreira <gustavobarreto2000@hotmail.com>
2023-07-20 21:37:04 -0300

    aula14 ex 017 concluido

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,52 @@
-alert("Ola");
+
+function GerarTabuada() {
+    
+    //pegando numero do usuario
+    let numero = document.querySelector("input#id-numero")
+
+    //pegando a div res para mostra o resulstado
+    let res = document.querySelector("div#res");
+
+
+
+
+    //capturando a tag select
+    let tab = document.querySelector("select#seltab");
+
+
+
+    //inserindo a tag option dentro da tag que foi capturada antes
+
+
+    
+
+
+
+    if (numero.value.length == 0) {
+        alert("Por favor, digite um numero!");
+
+
+    } else {
+        //convertendo o numero que foi digitado pelo usuario
+        let numConvertido = Number(numero.value)
+        tab.innerHTML = ""
+        for (let i = 1; i <= 10; i++) {
+
+            //criando a tag option dinamicamente
+            let item = document.createElement("option");
+            item.text = `${numConvertido} x ${i} = ${numConvertido*i}`;
+            item.value = `tab${i}`
+    
+            tab.appendChild(item);
+            
+    
+    
+    
+        }
+
+    }
+
+
+
+    
+}
