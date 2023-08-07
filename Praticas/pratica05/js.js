7a6d4a58db8d541a01f283f13118b31372255ff6
Gustavo Ferreira <gustavobarreto2000@hotmail.com>
2023-08-07 18:01:18 -0300

    praticas

---
+++ Praticas/pratica05/js.js
@@ -0,0 +1,23 @@
+ let nameCompleto = "Gustavo Barreto de Campos Ferreira"
+// console.log(nameCompleto)
+
+// let nome = nameCompleto.split(" ")
+// console.log(nome[0])
+// let ultimoNome = nameCompleto.split(" ")
+// let tamanho = nameCompleto.split(" ").length - 1
+// console.log(ultimoNome[tamanho])
+
+
+// let letras = nameCompleto.split("")
+
+// //primeria letra
+// console.log(letras[0])
+// //ultima letra
+// console.log(letras[letras.length -1])
+
+const phrase = "Eu quero viver o Amor!"
+
+let  newPhrase = phrase.split(" ");
+
+let phraseWithUnderscore = newPhrase.join("_")
+console.log(phraseWithUnderscore)
